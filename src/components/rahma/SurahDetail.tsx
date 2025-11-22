ada6ecf99941284795b23fab3caf913c0ce769a1
Arfa Affu <arfaaffu2801@gmail.com>
2025-11-22 11:10:13 +0000

    remove this lines (_for element div_)

--- src/components/rahma/SurahDetail.tsx
+++ src/components/rahma/SurahDetail.tsx
@@ -78,11 +78,6 @@ const alBaqarahVerses = [
         arabic: 'أُولَٰئِكَ عَلَىٰ هُدًى مِنْ رَبِّهِمْ ۖ وَأُولَٰئِكَ هُمُ الْمُفْلِحُونَ',
         translation: 'Those are upon [right] guidance from their Lord, and it is those who are the successful.'
     },
-    {
-        verse: 257,
-        arabic: 'اللَّهُ وَلِيُّ الَّذِينَ آمَنُوا يُخْرِجُهُمْ مِنَ الظُّلُمَاتِ إِلَى النُّورِ ۖ وَالَّذِينَ كَفَرُوا أَوْلِيَاؤُهُمُ الطَّاغُوتُ يُخْرِجُونَهُمْ مِنَ النُّورِ إِلَى الظُّلُمَاتِ ۗ أُولَٰئِكَ أَصْحَابُ النَّارِ ۖ هُمْ فِيهَا خَالِدُونَ',
-        translation: "Allah is the ally of those who believe. He brings them out from darknesses into the light. And those who disbelieve - their allies are Taghut. They take them out of the light into darknesses. Those are the companions of the Fire; they will abide eternally therein."
-    }
 ];
 
 const surahContent: { [key: number]: any[] } = {
