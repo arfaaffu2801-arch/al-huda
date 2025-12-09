0474975136d812ccbd7174ac4be902596f177617
Arfa Affu <arfaaffu2801@gmail.com>
2025-12-09 11:28:37 +0000

    add this all in down on this the shahda (_for element <QuickActions>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
     <div className="flex min-h-screen w-full flex-col bg-background font-body">
       <Header />
       <main className="flex flex-1 flex-col gap-6 p-4 sm:p-6 md:p-8">
-        <div className="mx-auto w-full max-w-screen-2xl">
-          <QuickActions />
-        </div>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mx-auto w-full max-w-screen-2xl">
           <Shahada />
+        </div>
+        <div className="mx-auto w-full max-w-screen-2xl">
+          <QuickActions />
         </div>
       </main>
     </div>
