95329e664e5e7cadef5d291d7ad0eb876e4a6603
Arfa Affu <arfaaffu2801@gmail.com>
2026-01-08 11:05:10 +0000

    Try fixing this error: `Console Error: Invalid DOM property `stroke-widt

--- src/components/rahma/NamazIcon.tsx
+++ src/components/rahma/NamazIcon.tsx
@@ -11,10 +11,10 @@ export function NamazIcon({ className }: { className?: string }) {
     >
         <path d="M40,14H8c-2.2,0-4,1.8-4,4v20c0,2.2,1.8,4,4,4h32c2.2,0,4-1.8,4-4V18C44,15.8,42.2,14,40,14z" fill="#5BE4AE"/>
         <path d="M38,6H10c-2.2,0-4,1.8-4,4v4h36v-4C42,7.8,40.2,6,38,6z" fill="#00A76F"/>
-        <path d="M14,4v4" stroke="#FFF" stroke-width="2" stroke-linecap="round"/>
-        <path d="M34,4v4" stroke="#FFF" stroke-width="2" stroke-linecap="round"/>
+        <path d="M14,4v4" stroke="#FFF" strokeWidth="2" strokeLinecap="round"/>
+        <path d="M34,4v4" stroke="#FFF" strokeWidth="2" strokeLinecap="round"/>
         <path d="M24,22c-4.4,0-8,3.6-8,8s3.6,8,8,8s8-3.6,8-8S28.4,22,24,22z" fill="#FFC107"/>
-        <path d="M24,24v6h4" stroke="#FFF" stroke-width="2" stroke-linecap="round"/>
+        <path d="M24,24v6h4" stroke="#FFF" strokeWidth="2" strokeLinecap="round"/>
     </svg>
   );
 }
